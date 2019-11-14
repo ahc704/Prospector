906a270acf878c3dd287b9a67f6b1f880142e33f
ahc704 <46978022+ahc704@users.noreply.github.com>
2019-11-13 22:50:48 -0500

    Floating Score Script Created

---
+++ Assets/__Scripts/FloatingScore.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum eFSState
+{
+    idle,
+    pre,
+    active,
+    post
+}
+
+public class FloatingScore : MonoBehaviour
+{
+    [Header("Set Dynamically")]
+    public eFSState state = eFSState.idle;
+
+    [SerializeField]
+    protected int _score = 0;
+    public string scoreString;
+    public int score
+    {
+        get {
+            return (_score);
+        }
+        set
+        {
+            _score = value;
+            scoreString = _score.ToString("N0");
+        }
+    }
+}
