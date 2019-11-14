5972d70e947062ad93aa6ed1993739b26e8b323e
ahc704 <46978022+ahc704@users.noreply.github.com>
2019-11-14 09:39:04 -0500

    Announcing the Beginning and End of Rounds

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -19,6 +19,7 @@ public class Prospector : MonoBehaviour {
     public Vector2 fsPosRun = new Vector2(0.5f, 0.75f);
     public Vector2 fsPosMid2 = new Vector2(0.4f, 1.0f);
     public Vector2 fsPosEnd = new Vector2(0.5f, 0.95f);
+    public float reloadDelay = 2f;
 
 	[Header("Set Dynamically")]
 	public Deck	deck;
@@ -269,7 +270,12 @@ public class Prospector : MonoBehaviour {
             ScoreManager.EVENT(eScoreEvent.gameLoss);
             FloatingScoreHandler(eScoreEvent.gameLoss);
         }
-        SceneManager.LoadScene("_Prospector_Scene_0");
+        Invoke("ReloadLevel", reloadDelay);
+    }
+
+    void ReloadLevel()
+    {
+        SceneManager.LoadScene("__Prospector_Scene_0");
     }
 
     public bool AdjacentRank(CardProspector c0, CardProspector c1)
