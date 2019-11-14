ccc18b2c94065df9366776dd5bb13ca67790a7b0
ahc704 <46978022+ahc704@users.noreply.github.com>
2019-11-14 00:11:57 -0500

    Showing the Score to the Players

--- Assets/__Scripts/FloatingScore.cs
+++ Assets/__Scripts/FloatingScore.cs
@@ -28,6 +28,87 @@ public class FloatingScore : MonoBehaviour
         {
             _score = value;
             scoreString = _score.ToString("N0");
+            GetComponent<Text>().text = scoreString;
+        }
+    }
+
+    public List<Vector2> bezierPts;
+    public List<float> fontSizes;
+    public float timeStart = -1f;
+    public float timeDuration = 1f;
+    public string easingCurve = Easing.InOut;
+
+    public GameObject reportFinishTo = null;
+
+    private RectTransform rectTrans;
+    private Text txt;
+
+    public void Init(List<Vector2> ePts, float eTimeS = 0, float eTimeD = 1)
+    {
+        rectTrans = GetComponent<RectTransform>();
+        rectTrans.anchoredPosition = Vector2.zero;
+
+        txt = GetComponent<Text>();
+        bezierPts = new List<Vector2>(ePts);
+
+        if (ePts.Count == 1)
+        {
+            transform.position = ePts[0];
+            return;
+        }
+
+        if (eTimeS == 0) eTimeS = Time.time;
+            timeStart = eTimeS;
+            timeDuration = eTimeD;
+
+        state = eFSState.pre;
+    }
+
+    public void FSCallback(FloatingScore fs)
+    {
+        score += fs.score;
+    }
+
+
+    void Update()
+    {
+        if (state == eFSState.idle) return;
+
+        float u = (Time.time - timeStart) / timeDuration;
+        float uC = Easing.Ease(u, easingCurve);
+        if (u < 0){
+            state = eFSState.pre;
+            txt.enabled = false;
+        }
+        else
+        {
+            if (u >= 1)
+            {
+                uC = 1;
+                state = eFSState.pre;
+                if(reportFinishTo != null)
+                {
+                    reportFinishTo.SendMessage("FSCallback", this);
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    state = eFSState.idle;
+                }
+            }
+            else
+            {
+                state = eFSState.active;
+                txt.enabled = true;
+            }
+
+            Vector2 pos = Utils.Bezier(uC, bezierPts);
+            rectTrans.anchorMin = rectTrans.anchorMax = pos;
+            if (fontSizes != null && fontSizes.Count > 0)
+            {
+                int size = Mathf.RoundToInt(Utils.Bezier(uC, fontSizes));
+                GetComponent<Text>().fontSize = size;
+            }
         }
     }
 }
