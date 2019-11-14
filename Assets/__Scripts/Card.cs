8fea839d30a29c9f2209c87c425f81c8c226de2a
ahc704 <46978022+ahc704@users.noreply.github.com>
2019-11-13 21:02:10 -0500

    Inital Mine Layout Added

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -2,41 +2,84 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Card : MonoBehaviour {
+public class Card : MonoBehaviour
+{
 
-	public string    suit;
-	public int       rank;
-	public Color     color = Color.black;
-	public string    colS = "Black";  // or "Red"
-	
-	public List<GameObject> decoGOs = new List<GameObject>();
-	public List<GameObject> pipGOs = new List<GameObject>();
-	
-	public GameObject back;  // back of card;
-	public CardDefinition def;  // from DeckXML.xml		
+    public string suit;
+    public int rank;
+    public Color color = Color.black;
+    public string colS = "Black";  // or "Red"
 
+    public List<GameObject> decoGOs = new List<GameObject>();
+    public List<GameObject> pipGOs = new List<GameObject>();
 
-	public bool faceUp {
-		get {
-			return (!back.activeSelf);
-		}
+    public GameObject back;  // back of card;
+    public CardDefinition def;  // from DeckXML.xml		
 
-		set {
-			back.SetActive(!value);
-		}
-	}
+    public SpriteRenderer[] spriteRenderers;
 
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-} // class Card
+    public bool faceUp
+    {
+        get
+        {
+            return (!back.activeSelf);
+        }
+
+        set
+        {
+            back.SetActive(!value);
+        }
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        setSortOrder(0);
+    }
+
+    public void PopulateSpriteRenders()
+    {
+        if (spriteRenderers == null || spriteRenderers.Length == 0)
+        {
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+    }
+
+    public void SetSortingLayerNames(string tSLN)
+    {
+        PopulateSpriteRenders();
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            tSR.sortingLayerName = tSLN;
+        }
+    }
+
+    public void setSortOrder(int sOrd)
+    {
+        PopulateSpriteRenders();
+        foreach(SpriteRenderer tSR in spriteRenderers)
+        {
+            if (tSR.gameObject == this.gameObject)
+            {
+                tSR.sortingOrder = sOrd;
+                continue;
+            }
+
+            switch (tSR.gameObject.name)
+            {
+                case "back":
+                    tSR.sortingOrder = sOrd + 2;
+                    break;
+                case "face":
+                    tSR.sortingOrder = sOrd + 1;
+                    break;
+            }
+        }
+    }
+
+}// class Card
 
 [System.Serializable]
 public class Decorator{
